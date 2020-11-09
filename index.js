ddff22e705429e48244ea85d9ba4845c2af17eb9
Ferdian <ferdian.artwork@gmail.com>
2020-11-09 11:02:36 +0000

    pager done

--- index.js
+++ index.js
@@ -1,28 +1,98 @@
 const axios = require('axios');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const { sleep } = require('sleep-async')().Promise;
+
+const WIKI = axios.create({
+	baseURL: 'http://aqwwiki.wikidot.com/',
+	timeout: 20000,
+});
 
 (async () => {
 	try {
 
-		const result = await axios.get('http://aqwwiki.wikidot.com/wands');
-		const dom = new JSDOM(result.data);
+		
+		let dom = await getDom("staffs");
+		let page = dom.window.document.querySelector("#page-content");
 
-		const page = dom.window.document.querySelector("#page-content");
-		const box = page.querySelectorAll(".list-pages-box");
+		// find pagination number
+		let pager_max = 0;
+		const pager_dom = page.querySelectorAll(".pager-no");
+		for (let i=0; i<pager_dom.length; i++) {
+			pager_max = Math.max(parseInt(pager_dom[i].textContent.split(" ").pop()), pager_max);
+		}
 
-		console.log(`There is ${box.length} list-pages-box`);
+		console.log(`Max paginatoion is ${pager_max}`);
 
-		for (let i=0; i<box.length; i++) {
-			const item = box[i].querySelectorAll(".list-pages-item");
+		const box_array = [];
 
-			console.log(`\n\nThere is ${item.length} list-pages-item in box ${i}`);
+		for (let pager=0; pager<=pager_max; pager++) {
 
-			for (let y=0; y<item.length; y++) {
-				const itemName = item[y].querySelector("p").textContent;
-				console.log(itemName);
+			await sleep(3000);
+
+			// load new pager
+			if (pager != 0) {
+				dom = await getDom(`staffs/p/${pager}`);
+				page = dom.window.document.querySelector("#page-content");
+			}
+
+			const box = page.querySelectorAll(".list-pages-box");
+			console.log(`There is ${box.length} box`);
+
+			for (let i=0; i<box.length; i++) {
+				box_array[i] = [];
+
+				// check current pager
+				const pager_curr = box[i].querySelector(".pager-no");
+				// console.log(pager_curr);
+
+				// scrap first pager
+				if (pager_curr == null && pager == 0) {
+
+					const item = box[i].querySelectorAll(".list-pages-item");
+					console.log(`(${i}) There is ${item.length} item`);
+					
+					for (let y=0; y<item.length; y++) {
+						const itemName = item[y].querySelector("p").textContent;
+
+						box_array[i].push(itemName);
+					}
+
+				} else if (pager_curr != null && pager != 0) {
+
+					pager_currNum = parseInt(pager_curr.textContent.split(" ")[1]);
+					if (pager_currNum == pager) {
+						const item = box[i].querySelectorAll(".list-pages-item");
+						console.log(`(${i}) There is ${item.length} item`);
+
+						for (let y=0; y<item.length; y++) {
+							const itemName = item[y].querySelector("p").textContent;
+
+							box_array[i].push(itemName);
+						}
+					}
+
+				}
+
 			}
 		}
+
+		console.log("done");
+
+		// const box = page.querySelectorAll(".list-pages-box");
+
+		// console.log(`There is ${box.length} list-pages-box`);
+
+		// for (let i=0; i<box.length; i++) {
+		// 	const item = box[i].querySelectorAll(".list-pages-item");
+
+		// 	console.log(`There is ${item.length} list-pages-item in box ${i}`);
+
+		// 	for (let y=0; y<item.length; y++) {
+		// 		const itemName = item[y].querySelector("p").textContent;
+		// 		console.log(itemName);
+		// 	}
+		// }
 
 		// console.log(box[2].querySelectorAll(".list-pages-item")[1].querySelector("p").textContent)
 
@@ -31,3 +101,13 @@ const { JSDOM } = jsdom;
 	}
 
 })();
+
+async function getDom(link) {
+	try {
+		console.log(`Loading ${link}...`);
+		const result = await WIKI.get(link);
+		return new JSDOM(result.data);
+	} catch(e) {
+		throw new Error("Failed to fetch dom");
+	}
+}
